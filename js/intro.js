765ef68cde55482f8911f24e4c9669fe4bce32ed
James Slattery <james@slattery.tech>
2017-05-08 11:53:56 -0700

    intro.js

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+    $('.quintus_container').hide();
     $('.play').click(function () {
         $('.intro').fadeOut('slow');
+        $('.quintus_container').fadeIn('fast');
     })
 });
