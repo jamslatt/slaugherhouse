34fe071e4c0430ac72dffdec6c976ace4a01429c
James Slattery <james@slattery.tech>
2017-05-15 11:15:33 -0700

    S5 game.js

--- js/game.js
+++ js/game.js
@@ -32,8 +32,8 @@ window.addEventListener("load",function() {
             // You can call the parent's constructor with this._super(..)
             this._super(p, {
                 sheet: "player",  // Setting a sprite sheet sets sprite width and height
-                x: 410,           // You can also set additional properties that can
-                y: 90             // be overridden on object creation
+                x: 1300,           // You can also set additional properties that can
+                y: 300             // be overridden on object creation
             });
 
             // Add in pre-made components to get up and running quickly
